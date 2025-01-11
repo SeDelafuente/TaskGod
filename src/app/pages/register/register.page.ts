13e43e5f6ab409a535d73debdc0590489bc39898
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-11 18:57:04 -0300

    Página register, scss listo y estructura general

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -4,21 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class RegisterPage implements OnInit {
-
   email: string = '';
   password: string = '';
   passwordType: string = 'password'; // Por defecto oculta la contraseña
   confirmPassword: string = '';
-  passwordConfirmType: string = 'password'; // Por defecto oculta la contraseña de confirmación
+  confirmPasswordType: string = 'password'; // Por defecto oculta la contraseña de confirmación
   name: string = '';
   userName: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // Método para alternar la visibilidad de la contraseña
+  togglePasswordType() {
+    this.passwordType =
+      this.passwordType === 'password' ? 'text' : 'password';
   }
 
+  // Método para alternar la visibilidad de la confirmación de contraseña
+  toggleConfirmPasswordType() {
+    this.confirmPasswordType =
+      this.confirmPasswordType === 'password' ? 'text' : 'password';
+  }
 }
