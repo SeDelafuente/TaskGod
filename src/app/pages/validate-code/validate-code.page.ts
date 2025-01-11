9c8690893201ce8642331eeabbf162cd52e6969f
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-11 20:27:35 -0300

    ValidateCode - scss y estructura

--- src/app/pages/validate-code/validate-code.page.ts
+++ src/app/pages/validate-code/validate-code.page.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-validate-code',
   templateUrl: './validate-code.page.html',
   styleUrls: ['./validate-code.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class ValidateCodePage implements OnInit {
+  code: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  redirectToLogin(){
+    this.router.navigate(['/login'])
   }
 
 }
