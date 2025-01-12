ed89cfab03ede4430f697a8e8a8128feea096098
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-12 04:06:59 -0300

    Profile - scss y estructura

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class ProfilePage implements OnInit {
+  // Variables para el perfil del usuario
+  username: string = '$USERNAME';
+  email: string = 'usuario@example.com';
+  password: string = '**********';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.loadUserProfile();
   }
 
+  // Cargar los datos del perfil del usuario
+  loadUserProfile() {
+    this.username = 'Username';
+    this.email = 'username@example.com';
+    this.password = '**********';
+  }
+
+  // Navegar a la página de cambio de contraseña
+  goToChangePassword() {
+    this.router.navigate(['/change-password']);
+  }
+
+  // Navegar a la página de cambio de nombre de usuario
+  goToChangeUsername() {
+    this.router.navigate(['/change-username']);
+  }
+
+  // Navegar a la página de ayuda
+  goToHelp(){
+    this.router.navigate(['/help']);
+  }
+
+  // Cerrar sesión y navegar a la página de inicio
+  goToLogin(){
+    this.router.navigate(['/login']);
+  }
 }
