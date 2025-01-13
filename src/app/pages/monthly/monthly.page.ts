c71bf313f48b83944ba955d7b1a847bab0917d4b
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 01:48:02 -0300

    Monthly - scss y estructura

--- src/app/pages/monthly/monthly.page.ts
+++ src/app/pages/monthly/monthly.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-monthly',
@@ -6,11 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monthly.page.scss'],
   standalone: false
 })
-export class MonthlyPage implements OnInit {
+export class MonthlyPage {
+  tasks = [
+    { description: 'Terminar mes con $100.000', completed: false },
+    { description: 'Hacer ejercicio todas las semanas', completed: false },
+    { description: 'Estar al día con informes', completed: false },
+    { description: 'Hacer rutina de skincare todas las semanas', completed: false },
+  ];
 
-  constructor() { }
+  newTask: string = '';
+  isModalOpen = false;
 
-  ngOnInit() {
+  currentMonth: string = new Date().toLocaleString('es-ES', { month: 'long' });
+
+  openModal() {
+    this.isModalOpen = true;
   }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ description: this.newTask.trim(), completed: false });
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
