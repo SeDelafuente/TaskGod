b387828a30960133d081ca9f9cb36391bd7e366c
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 03:08:55 -0300

    Economic - scss y estructura parcial

--- src/app/pages/economic/economic.module.ts
+++ src/app/pages/economic/economic.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { EconomicPageRoutingModule } from './economic-routing.module';
-
 import { EconomicPage } from './economic.page';
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EconomicPageRoutingModule
+    EconomicPageRoutingModule,
   ],
   declarations: [EconomicPage]
 })
--- src/app/pages/economic/economic.page.ts
+++ src/app/pages/economic/economic.page.ts
@@ -1,16 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-economic',
   templateUrl: './economic.page.html',
   styleUrls: ['./economic.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class EconomicPage implements OnInit {
+  savingsGoal: number = 500000; // Meta de ahorro inicial
+  currentSavings: number = 200000; // Cantidad ahorrada inicial
+  currentMonth: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.currentMonth = new Date().toLocaleDateString('es-CL', {
+      month: 'long',
+    });
   }
 
+  goToIncome() {
+    this.router.navigate(['/income']);
+  }
+
+  goToExpenses() {
+    this.router.navigate(['/expenses']);
+  }
+
+  goToSavings() {
+    this.router.navigate(['/savings']);
+  }
+
+  goToWithdraw() {
+    this.router.navigate(['/withdraw']);
+  }
+
+  goToHistory() {
+    this.router.navigate(['/history']);
+  }
+
+  goToHelp() {
+    this.router.navigate(['/help']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  editSavingsGoal() {
+    console.log('Editar meta de ahorro');
+  }
+
+  updateSavings() {
+    console.log('Actualizar cantidad ahorrada');
+  }
 }
