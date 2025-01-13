0ff58120d0f4a73b0577c4aa502d24a62829dd18
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 05:35:50 -0300

    Problems - scss y estructura

--- src/app/pages/problems/problems.page.ts
+++ src/app/pages/problems/problems.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-problems',
@@ -6,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./problems.page.scss'],
   standalone: false
 })
-export class ProblemsPage implements OnInit {
+export class ProblemsPage {
+  problems = [
+    { userId: 'User123', title: 'Problema ingreso datos' },
+    { userId: 'User456', title: 'Error en visualización' },
+    { userId: 'User789', title: 'No puedo acceder' },
+    { userId: 'User321', title: 'Problema carga archivos' },
+    { userId: 'User654', title: 'Solicitud de ayuda' },
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  viewProblem(problem: any) {
+    alert(`Detalles del problema:\nUsuario: ${problem.userId}\nTítulo: ${problem.title}`);
   }
 
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
