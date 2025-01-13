9dbf670c59b9ec963f45a8a0d94f8b8545e22e2b
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 01:37:09 -0300

    Diaary - Scss y estructura

--- src/app/pages/diary/diary.page.ts
+++ src/app/pages/diary/diary.page.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-diary',
   templateUrl: './diary.page.html',
   styleUrls: ['./diary.page.scss'],
-  standalone: false
+  standalone: false,
 })
-export class DiaryPage implements OnInit {
+export class DiaryPage {
+  tasks = [
+    { description: 'Poner en ahorro $2,500', completed: false },
+    { description: 'Hacer rutina de cardio', completed: false },
+    { description: 'Entregar informe', completed: false },
+    { description: 'Meditar 15 min', completed: false },
+  ];
 
-  constructor() { }
+  newTask: string = '';
+  isModalOpen = false;
 
-  ngOnInit() {
+  openModal() {
+    this.isModalOpen = true;
   }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ description: this.newTask.trim(), completed: false });
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
+
