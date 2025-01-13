94cfaf2cc0ce42b3b5d6295a58eb3579cf85bbef
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 15:41:32 -0300

    Register - Validaciones usando validator

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
@@ -12,6 +11,7 @@ import { RegisterPage } from './register.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RegisterPageRoutingModule
   ],
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,33 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class RegisterPage implements OnInit {
-  email: string = '';
-  password: string = '';
+  registerForm!: FormGroup;
   passwordType: string = 'password'; // Por defecto oculta la contraseña
-  confirmPassword: string = '';
-  confirmPasswordType: string = 'password'; // Por defecto oculta la contraseña de confirmación
-  name: string = '';
-  userName: string = '';
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]+$'),
+          Validators.maxLength(30),
+        ],
+      ],
+      username: ['', [Validators.required, Validators.maxLength(30)]],
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.maxLength(30)],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$'),
+        ],
+      ],
+    });
+  }
 
-  // Método para alternar la visibilidad de la contraseña
   togglePasswordType() {
     this.passwordType =
       this.passwordType === 'password' ? 'text' : 'password';
   }
 
-  // Método para alternar la visibilidad de la confirmación de contraseña
-  toggleConfirmPasswordType() {
-    this.confirmPasswordType =
-      this.confirmPasswordType === 'password' ? 'text' : 'password';
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
   }
 }
