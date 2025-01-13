3dc2c355d289940d9c99312da74b2fcf7c3d7855
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 15:58:25 -0300

    Help - Validaciones usando Validator

--- src/app/pages/help/help.page.ts
+++ src/app/pages/help/help.page.ts
@@ -1,49 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.page.html',
   styleUrls: ['./help.page.scss'],
-  standalone: false
+  standalone: false,
 })
-export class HelpPage {
-  form = {
-    problemSummary: '',
-    problemDetails: '',
-    incidentDate: '',
-    affectedPages: '',
-  };
+export class HelpPage implements OnInit {
+  helpForm: FormGroup;
 
-  constructor(private router: Router) {}
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.helpForm = this.fb.group({
+      problemSummary: [
+        '',
+        [Validators.required, Validators.maxLength(30)],
+      ],
+      problemDetails: ['', Validators.required],
+      incidentDate: ['', Validators.required],
+      affectedPages: [''],
+    });
+  }
+
+  ngOnInit(): void {}
 
   goToLogin() {
     this.router.navigate(['/login']);
   }
 
   submitForm() {
-    if (this.isFormValid()) {
-      console.log('Formulario enviado:', this.form);
+    if (this.helpForm.valid) {
+      console.log('Formulario enviado:', this.helpForm.value);
       alert('Formulario enviado correctamente');
-      this.resetForm();
+      this.helpForm.reset(); // Reinicia el formulario
     } else {
       alert('Por favor, completa todos los campos requeridos.');
     }
-  }
-
-  isFormValid() {
-    return (
-      this.form.problemSummary.trim() !== '' &&
-      this.form.problemDetails.trim() !== ''
-    );
-  }
-
-  resetForm() {
-    this.form = {
-      problemSummary: '',
-      problemDetails: '',
-      incidentDate: '',
-      affectedPages: '',
-    };
   }
 }
