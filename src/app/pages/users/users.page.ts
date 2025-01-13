3bf8644065690d28b4ad5112e89641a87e8c5c2e
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 05:27:36 -0300

    Users - scss y estructura

--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.page.scss'],
   standalone: false
 })
-export class UsersPage implements OnInit {
+export class UsersPage implements OnInit{
+  ngOnInit(): void {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  users = [
+    { id: '1', username: 'usuario1' },
+    { id: '2', username: 'usuario2' },
+    { id: '3', username: 'usuario3' },
+    { id: '4', username: 'usuario4' },
+    { id: '5', username: 'usuario5' },
+  ];
+
+  constructor(private router: Router) {}
+
+  blockUser(user: any) {
+    alert(`Usuario ${user.username} bloqueado.`);
+    // Aquí se implementaría la lógica para bloquear al usuario.
+  }
+
+  deleteUser(user: any) {
+    const index = this.users.indexOf(user);
+    if (index > -1) {
+      this.users.splice(index, 1);
+      alert(`Usuario ${user.username} eliminado.`);
+    }
+  }
+
+  goToHelp() {
+    this.router.navigate(['/help']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
