4c339b565d35dde882b90a36141ff5dfa295f6d5
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 02:31:44 -0300

    Selfcare - SCSS y estructura

--- src/app/pages/selfcare/selfcare.page.ts
+++ src/app/pages/selfcare/selfcare.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-selfcare',
@@ -7,10 +8,71 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class SelfcarePage implements OnInit {
+  dailyTasks = [
+    { description: 'Meditar 15 min', completed: false },
+  ];
 
-  constructor() { }
+  monthlyTasks = [
+    { description: 'Hacer skincare todas las semanas', completed: false },
+  ];
 
-  ngOnInit() {
+  newDailyTask: string = '';
+  newMonthlyTask: string = '';
+
+  isDailyModalOpen = false;
+  isMonthlyModalOpen = false;
+
+  constructor(private router: Router) {}
+
+  openDailyModal() {
+    this.isDailyModalOpen = true;
   }
 
+  closeDailyModal() {
+    this.isDailyModalOpen = false;
+  }
+
+  addDailyTask() {
+    if (this.newDailyTask.trim()) {
+      this.dailyTasks.push({ description: this.newDailyTask.trim(), completed: false });
+      this.newDailyTask = '';
+    }
+  }
+
+  deleteDailyTask(index: number) {
+    this.dailyTasks.splice(index, 1);
+  }
+
+  openMonthlyModal() {
+    this.isMonthlyModalOpen = true;
+  }
+
+  closeMonthlyModal() {
+    this.isMonthlyModalOpen = false;
+  }
+
+  addMonthlyTask() {
+    if (this.newMonthlyTask.trim()) {
+      this.monthlyTasks.push({ description: this.newMonthlyTask.trim(), completed: false });
+      this.newMonthlyTask = '';
+    }
+  }
+
+  deleteMonthlyTask(index: number) {
+    this.monthlyTasks.splice(index, 1);
+  }
+
+  goToArticles() {
+    this.router.navigate(['/articles']);
+  }
+
+  goToHelp() {
+    this.router.navigate(['/help']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  ngOnInit(): void {}
 }
