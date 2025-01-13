7f6586093eca1d8c11bb1f3ba65b9b7f4f7a6007
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 04:33:28 -0300

    Withdraw - scss y estructura

--- src/app/pages/withdraw/withdraw.page.ts
+++ src/app/pages/withdraw/withdraw.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-withdraw',
@@ -8,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WithdrawPage implements OnInit {
 
-  constructor() { }
+  currentDate: string = '';
+  incomeAmount: string = '';
+  keypad: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',];
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.currentDate = new Date().toLocaleDateString('es-CL', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  }
+
+  onKeyPress(key: string) {
+    if (key === '=') {
+      // Lógica para evaluar expresiones matemáticas
+      try {
+        this.incomeAmount = eval(this.incomeAmount).toString();
+      } catch {
+        this.incomeAmount = '0';
+      }
+    } else if (key === 'x') {
+      this.incomeAmount += '*'; // Representar multiplicación
+    } else if (key === '/') {
+      this.incomeAmount += '/'; // Representar división
+    } else {
+      this.incomeAmount += key;
+    }
+  }
+
+  clearInput() {
+    this.incomeAmount = '';
+  }
+
+  chooseCategory() {
+    this.router.navigate(['/economic']);
+  }
+
+  goToHelp() {
+    this.router.navigate(['/help']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 
 }
+
