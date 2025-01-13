dd13fe9bdd8240df75271d6a5d804250299e1d8b
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-13 15:49:47 -0300

    Forget-password - Validaciones usando validators

--- src/app/pages/forget-password/forget-password.page.ts
+++ src/app/pages/forget-password/forget-password.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-forget-password',
   templateUrl: './forget-password.page.html',
@@ -7,13 +9,28 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class ForgetPasswordPage implements OnInit {
-  email: string = '';
+  forgetPasswordForm: FormGroup;
 
-  constructor(private router: Router) {}
-
-  goToValidate(){
-    this.router.navigate(['/validate-code'])
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.forgetPasswordForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.maxLength(30),
+        ],
+      ],
+    });
   }
-  ngOnInit() {}
-}
 
+  ngOnInit() {}
+
+  onSubmit() {
+    if (this.forgetPasswordForm.valid) {
+      // Aquí procesamos el envío
+      console.log('Formulario enviado con éxito:', this.forgetPasswordForm.value);
+      this.router.navigate(['/validate-code']);
+    }
+  }
+}
