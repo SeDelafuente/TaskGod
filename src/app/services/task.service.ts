36d5286a14e55721d016cdf0c00f92e328891c82
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-26 23:32:12 -0300

    Creacion de TaskService

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,76 @@
 import { Injectable } from '@angular/core';
-import { task } from '../models/task.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { task } from '../models/task.model';
+import { Observable, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-  constructor(private firestore: AngularFirestore, private authService: AuthService) {}
+  constructor(
+    private firestore: AngularFirestore,
+    private authService: AuthService
+  ) {}
 
-  addTask(){
-
+  // Crea un task
+  createTask(newTask: task): Promise<void> {
+    const taskId = this.firestore.createId();
+    return this.firestore
+      .collection('tasks')
+      .doc(taskId)
+      .set({ ...newTask, id: taskId });
   }
 
+  // Actualiza un task
+  updateTask(taskId: string, updatedFields: Partial<task>): Promise<void> {
+    return this.firestore.collection('tasks').doc(taskId).update(updatedFields);
+  }
 
+  // Borra un task
+  deleteTask(taskId: string): Promise<void> {
+    return this.firestore.collection('tasks').doc(taskId).delete();
+  }
+
+  // Consigue los task por tipo para el usuario actual
+  getTasksByType(tipo: string): Observable<task[]> {
+    return from(this.authService.getUserId()).pipe(
+      switchMap((userId) =>
+        this.firestore
+          .collection<task>('tasks', (ref) =>
+            ref.where('userId', '==', userId).where('tipo', '==', tipo)
+          )
+          .valueChanges()
+      )
+    );
+  }
+
+  // Conseguir Task por categoria y tipo para el usuario actual
+  getTasksByCategoryAndType(
+    category: string,
+    tipo: string
+  ): Observable<task[]> {
+    return from(this.authService.getUserId()).pipe(
+      switchMap((userId) =>
+        this.firestore
+          .collection<task>('tasks', (ref) =>
+            ref
+              .where('userId', '==', userId)
+              .where('category', '==', category)
+              .where('tipo', '==', tipo)
+          )
+          .valueChanges()
+      )
+    );
+  }
+
+  // cambiar estado de realizado a los task
+  markTaskAsCompleted(taskId: string): Promise<void> {
+    return this.updateTask(taskId, { isCompleted: true });
+  }
+
+  unmarkTaskAsCompleted(taskId: string): Promise<void> {
+    return this.updateTask(taskId, { isCompleted: false });
+  }
 }
