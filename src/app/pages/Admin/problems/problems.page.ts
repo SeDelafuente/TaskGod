a921e4cf3e3e56d6c36c5c149779d417becc0f2b
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-01-29 14:03:24 -0300

    Problem details in ionAlerts

--- src/app/pages/Admin/problems/problems.page.ts
+++ src/app/pages/Admin/problems/problems.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SupportService } from 'src/app/services/support.service';
 import { HelpForm } from 'src/app/models/helpForm.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-problems',
@@ -12,7 +13,11 @@ import { HelpForm } from 'src/app/models/helpForm.model';
 export class ProblemsPage implements OnInit {
   problems: HelpForm[] = []; // Lista para almacenar los problemas cargados
 
-  constructor(private router: Router, private supportService: SupportService) {}
+  constructor(
+    private router: Router,
+    private supportService: SupportService,
+    private alertController: AlertController // Inyectamos el AlertController
+  ) {}
 
   ngOnInit() {
     this.loadProblems(); // Cargar problemas al inicializar
@@ -25,11 +30,22 @@ export class ProblemsPage implements OnInit {
     });
   }
 
-  // Método para ver los detalles de un problema
-  viewProblem(problem: HelpForm) {
-    alert(
-      `Detalles del problema:\nUsuario: ${problem.userid}\nResumen: ${problem.problemSummary}\nDetalles: ${problem.problemDetails}`
-    );
+  // Método para ver los detalles de un problema usando ion-alert
+  async viewProblem(problem: HelpForm) {
+    const alert = await this.alertController.create({
+      header: 'Detalles del problema',
+      message: `
+        Usuario: ${problem.userid}
+        Resumen: ${problem.problemSummary}
+        Detalles: ${problem.problemDetails}
+        Fecha: ${problem.incidentDate}
+        Estado: ${problem.status}
+      `,
+      buttons: ['Cerrar'],
+      cssClass: 'custom-alert', // Agregar una clase CSS personalizada
+    });
+
+    await alert.present();
   }
 
   // Método para navegar al login
